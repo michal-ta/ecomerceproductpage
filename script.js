8f12d98951b1d9ad0b4209c0889898ef57ba5c9f
michal-ta <michaltakac93@gmail.com>
2022-07-20 14:49:27 +0200

    adding comments into JS

--- script.js
+++ script.js
@@ -3,16 +3,21 @@ let result =document.querySelector(".result");
 let quantityAdd=document.querySelector(".mark_plus");
 let quantityMinus=document.querySelector(".mark_minus");
 
+//changing numbers inside of box with total ''requested parts''
+//increase quanitty 
 quantityAdd.addEventListener("click", increment);
-    console.log(increment);
+        
 function increment(){
-    result.innerText=Number(result.innerText) + 1;
+    result.innerText=Number(result.innerText) +1;
 }
 
-
+//decrease quantity
 quantityMinus.addEventListener("click", decrease);
+
+//avoid to go into minus value 
 function decrease(){
     if (result.innerText > 0){
     result.innerText=Number(result.innerText) -1;
     }
 }
+
